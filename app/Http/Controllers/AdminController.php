56401d4b019820fd85dc3ee0c61b129337ca6d70
josephkristanto10 <josephkristanto10@gmail.com>
2024-07-22 14:27:54 +0700

    update 22 juli 2024

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -460,10 +460,10 @@ class AdminController extends Controller
             $discount_product = 0;
         }
         // $file_product = $request->file('gbr_product');
-        $tujuan_upload = public_path('images/product');
+        // $tujuan_upload = public_path('images/product');
 
         // $mystr = "product".base64_encode(date("Y:m:d H:i:s")).".".$file_product->getClientOriginalExtension();
-        $myproduk = Product::create(["names" => $nama_product, "descriptions" => $desc_product, "prices" => $harga_product, "discounts" => $discount_product, "stocks" => "1", "has_variants" => "1", "product_status" => "1", "status_product_delete" => "0","images" => "-", "product_category"=>$pilihan_category, "updated_at" => now(), "created_at" => now()]);
+        // $myproduk = Product::create(["names" => $nama_product, "descriptions" => $desc_product, "prices" => $harga_product, "discounts" => $discount_product, "stocks" => "1", "has_variants" => "1", "product_status" => "1", "status_product_delete" => "0","images" => "-", "product_category"=>$pilihan_category, "updated_at" => now(), "created_at" => now()]);
         $products_id = $myproduk->id;
         // foreach( $request->add_nama_produks as $myindex => $dat){
         //     $hideen_id = $request->id[$myindex];
@@ -478,7 +478,7 @@ class AdminController extends Controller
          
         // }
         // $file_product->move($tujuan_upload, $mystr);
-        return response()->json(['output' => $file_product]);
+        return response()->json(['output' => "ok"]);
     }
 
 
